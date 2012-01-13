bbcfabacf2ed7921c0836930a6f6910d0cbecb56
Devin <babb.devin@gmail.com>
2012-01-13 09:35:16 -0600

    Removed files

--- run_dev.sh
+++
@@ -1,2 +0,0 @@
-nohup /home/java/jdk1.6.0_14/bin/java -Xms128m -Xmx2048m -classpath /home/rscproject/RSCProject/lib/mina.jar:/home/rscproject/RSCProject/lib/pircbot.jar:/home/rscproject/RSCProject/lib/bsh.jar:/home/rscproject/RSCProject/lib/xpp3.jar:/home/rscproject/RSCProject/lib/slf4j.jar:/home/rscproject/RSCProject/lib/xstream.jar:/home/rscproject/RSCProject/lib/mysql-connector.jar:/home/rscproject/RSCProject/lib/hex-string.jar:/home/rscproject/RSCProject/conf/server/quests:/home/rscproject/RSCProject/rscd.jar rscproject.gs.Server world.xml p2p no > devserver.log&
-
