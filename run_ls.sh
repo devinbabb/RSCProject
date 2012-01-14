1f5a8ed18806d7f792feb75ed6e04bc183bea0c8
Devin <babb.devin@gmail.com>
2012-01-14 11:44:42 -0600

    Fixed building and now runs properly, now to test client side

--- run_ls.sh
+++ run_ls.sh
@@ -1,3 +1,3 @@
 #cp runls.log logsbackup/runls.log.`date +%F.%s`
-nohup java -Xms56m -Xmx128m -classpath /home/devin/Projects/RSCProject/lib/mina.jar:/home/devin/Projects/RSCProject/lib/xpp3.jar:/home/devin/Projects/RSCProject/lib/slf4j.jar:/home/devin/Projects/RSCProject/lib/xstream.jar:/home/devin/Projects/RSCProject/lib/mysql-connector.jar:/home/devin/Projects/RSCProject/lib/hex-string.jar:rscd.jar rscproject.ls.Server ls.conf > runls.log&
+java -Xms56m -Xmx128m -classpath /home/devin/RSCProject/lib/mina.jar:/home/devin/RSCProject/lib/xpp3.jar:/home/devin/RSCProject/lib/slf4j.jar:/home/devin/RSCProject/lib/xstream.jar:/home/devin/RSCProject/lib/mysql-connector.jar:/home/devin/RSCProject/lib/hex-string.jar:rscd.jar rscproject.ls.Server ls.conf > runls.log&
 
--- run_server.sh
+++ run_server.sh
@@ -1,5 +1,5 @@
-cd /home/rscproject/RSCProject/
-cp runserver.log logsbackup/runserver.log.`date +%F.%s`
-cp logs/mod.log logsbackup/mod.log.`date +%F`
-nohup /home/java/jdk1.6.0_14/bin/java -Xms128m -Xmx2048m -classpath /home/rscproject/RSCProject/lib/mina.jar:/home/rscproject/RSCProject/lib/pircbot.jar:/home/rscproject/RSCProject/lib/bsh.jar:/home/rscproject/RSCProject/lib/xpp3.jar:/home/rscproject/RSCProject/lib/slf4j.jar:/home/rscproject/RSCProject/lib/xstream.jar:/home/rscproject/RSCProject/lib/mysql-connector.jar:/home/rscproject/RSCProject/lib/hex-string.jar:/home/rscproject/RSCProject/conf/server/quests:/home/rscproject/RSCProject/rscd.jar rscproject.gs.Server world.xml p2p yes > runserver.log&
+#cd /home/rscproject/RSCProject/
+#cp runserver.log logsbackup/runserver.log.`date +%F.%s`
+#cp logs/mod.log logsbackup/mod.log.`date +%F`
+java -Xms128m -Xmx2048m -classpath /home/devin/RSCProject/lib/mina.jar:/home/devin/RSCProject/lib/pircbot.jar:/home/devin/RSCProject/lib/bsh.jar:/home/devin/RSCProject/lib/xpp3.jar:/home/devin/RSCProject/lib/slf4j.jar:/home/devin/RSCProject/lib/xstream.jar:/home/devin/RSCProject/lib/mysql-connector.jar:/home/devin/RSCProject/lib/hex-string.jar:/home/devin/RSCProject/conf/server/quests:/home/devin/RSCProject/rscd.jar rscproject.gs.Server world.xml p2p yes > runserver.log&
 
